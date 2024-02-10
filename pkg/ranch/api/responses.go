a3b7482a3bf38523e9b53f3bf17ae1fb91ff36cc
Craig O'Donnell <cbodonnell@comcast.net>
2024-02-10 12:10:44 -0500

    client tls cert and key in response

--- pkg/ranch/api/responses.go
+++ pkg/ranch/api/responses.go
@@ -14,9 +14,11 @@ type OIDCReponse struct {
 }
 
 type ClientResponse struct {
-	ClientID     string     `json:"client_id"`
-	ClientSecret string     `json:"client_secret,omitempty"`
-	CreatedAt    time.Time  `json:"created_at"`
-	UpdatedAt    time.Time  `json:"updated_at"`
-	LastUsedAt   *time.Time `json:"last_used_at,omitempty"`
+	ClientID      string     `json:"client_id"`
+	ClientSecret  string     `json:"client_secret,omitempty"`
+	ClientTLSCert string     `json:"client_tls_cert"`
+	ClientTLSKey  string     `json:"client_tls_key,omitempty"`
+	CreatedAt     time.Time  `json:"created_at"`
+	UpdatedAt     time.Time  `json:"updated_at"`
+	LastUsedAt    *time.Time `json:"last_used_at,omitempty"`
 }
